a8a1934349e10065a026c0e7b53a84449bf9c781
lukeduffy84 <lukeduffy84@gmail.com>
2021-05-30 16:33:24 +0000

    Further template edits

--- app.py
+++ app.py
@@ -14,8 +14,21 @@ def register():
 def login():
     return render_template('login.html')
 
+@app.route('/upload-recipe')
+def upload_recipe():
+    return render_template('upload_recipe.html')
 
+@app.route('/all_recipes')
+def all_recipes():
+    return render_template('all_recipes.html')
 
+@app.route('/remove_recipe')
+def remove_recipe():
+    return render_template('remove_recipe.html')
+
+@app.route('/change_recipe')
+def change_recipe():
+    return render_template('change_recipe.html')
 
 
 if __name__ == "__main__":
