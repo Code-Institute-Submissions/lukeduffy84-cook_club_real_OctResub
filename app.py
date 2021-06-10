3b26e2d33d539fa10e8035a80ea3181c06db7108
lukeduffy84 <lukeduffy84@gmail.com>
2021-06-10 15:56:57 +0000

    Turned debug off before submission

--- app.py
+++ app.py
@@ -275,4 +275,4 @@ def search_api():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,threaded=True, host='0.0.0.0', port=5000)
+    app.run(debug= False,threaded=True, host='0.0.0.0', port=5000)
